f9882bf7cf66d7360c5afc1d2985098834a6c5c9
VikUrn <viktoras.urna@gmail.com>
2022-04-06 13:56:01 +0300

    clock

---
+++ laikrodis/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace laikrodis
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write a hours");
+            int h = int.Parse(Console.ReadLine());
+
+            if (h > 12 || h < 0)
+            {
+                Console.WriteLine("Write a hours from 1 to 12");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Write a minut");
+            int m = int.Parse(Console.ReadLine());
+
+            if (m > 60 || m < 1)
+            {
+                Console.WriteLine("Write a minuts from 1 to 60");
+                Console.ReadLine();
+            }
+
+            int counta = ((h * 5) - m) * 6;
+
+            if (counta > 90)
+            {
+                counta = 180 - counta;
+            }
+
+            int countb = (m - (h * 5)) * 6;
+
+            if (countb > 90)
+            {
+                countb = 180 - countb;
+            }
+
+            if ((h * 5) > m)
+            {
+                Console.WriteLine("Answer");
+                Console.WriteLine(counta);
+            }
+            else
+            {
+                Console.WriteLine("Answer");
+                Console.WriteLine(countb);
+            }
+
+        }
+    }
+}
---
+++ laikrodisUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("laikrodisUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("laikrodisUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a7760b3-729f-4c0c-9d6e-6c6eedfb0dda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
